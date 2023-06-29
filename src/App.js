e0462c86e1617f2d2546047be72eca07511ade06
mayank dutta <smayankdutt@gmail.com>
2023-06-29 20:31:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [planet, setPlanet] = useState()
+  const [vehicle, setVehicle] = useState([])
+  const [token, setToken] = useState()
+ 
+
+  const getPlanets = async () => {
+    const data = await axios.get("https://findfalcone.geektrust.com/planets");
+    console.log("planets data: ", data.data);
+  };
+
+  const getVehicles = async () => {
+    const data = await axios.get("https://findfalcone.geektrust.com/vehicles");
+
+    for (let i of data.data) {
+      setVehicle([...vehicle, i]);
+    }
+
+    console.log("vehicles data: ", data.data);
+  };
+
+  const findingFalcon = async () => {
+    const data = await axios.post(
+      "https://findfalcone.geektrust.com/token",
+      {},
+      { headers: { "Accept": "application/json" } }
+    );
+    console.log("finding Falcon: ", data.data);
+  };
+
+  console.log('array yvehicle: ', vehicle);
+
+  useEffect(() => {
+    getPlanets();
+    getVehicles();
+    findingFalcon();
+  }, []);
+
+  return <div className="App">Hello world</div>;
 }
 
 export default App;
